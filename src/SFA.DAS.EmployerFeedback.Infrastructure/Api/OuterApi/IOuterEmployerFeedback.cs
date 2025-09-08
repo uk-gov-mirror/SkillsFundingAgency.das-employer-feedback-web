62ec5c9ad12479dbf6bed5741940442b59104b53
Phil Head <152269503+philhead1@users.noreply.github.com>
2025-09-08 15:05:50 +0100

    Refactor and implementation of EmployerFeedback outer

---
+++ src/SFA.DAS.EmployerFeedback.Infrastructure/Api/OuterApi/IOuterEmployerFeedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestEase;
+using SFA.DAS.EmployerFeedback.Infrastructure.Api.Requests;
+
+namespace SFA.DAS.EmployerFeedback.Infrastructure.Api.OuterApi
+{
+    internal interface IEmployerFeedbackOuterApi
+    {
+        [Get("/employer-feedback/healthcheck")]
+        Task<string> HealthCheck();
+
+
+        [Post("/employerfeeback")]
+        Task<> SubmitEmployerFeedback(EmployerFeedbackRequest request);
+
+        [Get("/attributes")]
+        Task<> GetAllAttributes();
+
+        [Get("/employerfeedback?accountid={id}&userref={UUID}")]
+        Task<> GetAllProviderFeedbackAndResult([Query] long id, [Query] Guid userRef);
+    }
+}
---
+++ src/SFA.DAS.EmployerFeedback.Infrastructure/Api/Requests/EmployerFeedbackRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerFeedback.Infrastructure.Api.Requests
+{
+    public class EmployerFeedbackRequest
+    {
+        public Guid UserRef { get; set; }
+
+        public long Ukprn { get; set; }
+
+        public long AccountId { get; set; }
+
+    }
+}
