fa9f08c3ac7ece2516ac73dd5792f226f664848e
Chris Woodcock <chris@humesoftware.com>
2025-10-31 10:51:20 +0000

    Using existing paramter name for feedback source

--- src/SFA.DAS.EmployerFeedback.Web/Models/Provider/ProviderSearchRequestModel.cs
+++ src/SFA.DAS.EmployerFeedback.Web/Models/Provider/ProviderSearchRequestModel.cs
@@ -1,10 +1,12 @@
-﻿using SFA.DAS.EmployerFeedback.Domain.Types;
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerFeedback.Domain.Types;
 using SFA.DAS.EmployerFeedback.Web.Paging;
 
 namespace SFA.DAS.EmployerFeedback.Web.Models.Provider
 {
     public class ProviderSearchRequestModel : ProviderSearchSortRequestModel
     {
+        [FromQuery(Name = "source")]
         public FeedbackSource FeedbackSource { get; set; } = FeedbackSource.AdHoc;
         public int PageIndex { get; set; } = PagingState.DefaultPageIndex;
     }
