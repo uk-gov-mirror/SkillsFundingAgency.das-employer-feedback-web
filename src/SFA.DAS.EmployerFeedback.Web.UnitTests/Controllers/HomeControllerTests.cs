ac4a7740e932af2387fec3aaf154fbf560bd26a6
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-11-12 14:43:12 +0000

    P2-2960-Created new service controller and added index page

--- src/SFA.DAS.EmployerFeedback.Web.UnitTests/Controllers/HomeControllerTests.cs
+++ src/SFA.DAS.EmployerFeedback.Web.UnitTests/Controllers/HomeControllerTests.cs
@@ -1,131 +1,49 @@
-﻿using System;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerFeedback.Web.Controllers;
-using SFA.DAS.EmployerFeedback.Web.Models.Home;
-using SFA.DAS.GovUK.Auth.Models;
-using SFA.DAS.GovUK.Auth.Services;
+using SFA.DAS.Employer.Shared.UI;
+using SFA.DAS.GovUK.Auth.Configuration;
 
 namespace SFA.DAS.EmployerFeedback.Web.UnitTests.Controllers
 {
     [TestFixture]
     public class HomeControllerTests
     {
-        private Mock<IConfiguration> _mockConfig;
-        private Mock<IStubAuthenticationService> _mockStubAuthService;
-        private Mock<IHttpContextAccessor> _mockContextAccessor;
-        private Mock<HttpContext> _mockHttpContext;
-        private Mock<IResponseCookies> _mockCookies;
-        private Mock<HttpResponse> _mockResponse;
+        private UrlBuilder _urlBuilder;
+        private Mock<IConfiguration> _configurationMock;
+        private Mock<IHttpContextAccessor> _contextAccessorMock;
+        private Mock<ILogger<HomeController>> _loggerMock;
         private HomeController _sut;
 
         [SetUp]
         public void Setup()
         {
-            _mockConfig = new Mock<IConfiguration>();
-            _mockStubAuthService = new Mock<IStubAuthenticationService>();
-            _mockContextAccessor = new Mock<IHttpContextAccessor>();
-            _mockHttpContext = new Mock<HttpContext>();
-            _mockResponse = new Mock<HttpResponse>();
-            _mockCookies = new Mock<IResponseCookies>();
+            _urlBuilder = new UrlBuilder("LOCAL");
+            _configurationMock = new Mock<IConfiguration>();
+            _contextAccessorMock = new Mock<IHttpContextAccessor>();
+            _loggerMock = new Mock<ILogger<HomeController>>();
 
-            _mockResponse.Setup(r => r.Cookies).Returns(_mockCookies.Object);
-            _mockHttpContext.Setup(h => h.Response).Returns(_mockResponse.Object);
-            _mockContextAccessor.Setup(c => c.HttpContext).Returns(_mockHttpContext.Object);
-
-            _sut = new HomeController(_mockConfig.Object, _mockStubAuthService.Object, _mockContextAccessor.Object);
+            _sut = new HomeController(
+                _urlBuilder,
+                _configurationMock.Object,
+                _contextAccessorMock.Object,
+                _loggerMock.Object
+            );
         }
 
         [TearDown]
         public void TearDown()
         {
-            _sut.Dispose();
+            _sut?.Dispose();
         }
 
         [Test]
-        public async Task SignOut_Should_Return_SignOutResult_With_Correct_Schemes_When_StubAuth_False()
-        {
-            // Arrange
-            var expectedToken = "id123";
-
-            var authServiceMock = new Mock<IAuthenticationService>();
-            authServiceMock
-                .Setup(a => a.AuthenticateAsync(_mockHttpContext.Object, It.IsAny<string>()))
-                .ReturnsAsync(AuthenticateResult.Success(
-                    ticket: new AuthenticationTicket(new ClaimsPrincipal(), new AuthenticationProperties
-                    {
-                        Items = { { ".Token.id_token", expectedToken } }
-                    },
-                    CookieAuthenticationDefaults.AuthenticationScheme)));
-
-            _mockHttpContext.Setup(h => h.RequestServices.GetService(typeof(IAuthenticationService)))
-                .Returns(authServiceMock.Object);
-            _mockConfig.Setup(c => c["StubAuth"]).Returns("false");
-
-            // Act
-            var result = await _sut.SignOut();
-
-            // Assert
-            var signOutResult = result.Should().BeOfType<SignOutResult>().Subject;
-            signOutResult.AuthenticationSchemes.Should().Contain(CookieAuthenticationDefaults.AuthenticationScheme);
-            signOutResult.AuthenticationSchemes.Should().Contain(OpenIdConnectDefaults.AuthenticationScheme);
-            signOutResult.Properties.Parameters["id_token"].Should().Be(expectedToken);
-        }
-
-        [Test]
-        public async Task SignOut_Should_Include_Only_Cookie_Scheme_When_StubAuth_True()
-        {
-            // Arrange
-            var expectedToken = "id123";
-
-            var authServiceMock = new Mock<IAuthenticationService>();
-            authServiceMock
-                .Setup(a => a.AuthenticateAsync(_mockHttpContext.Object, It.IsAny<string>()))
-                .ReturnsAsync(AuthenticateResult.Success(
-                    ticket: new AuthenticationTicket(new ClaimsPrincipal(), new AuthenticationProperties
-                    {
-                        Items = { { ".Token.id_token", expectedToken } }
-                    },
-                    CookieAuthenticationDefaults.AuthenticationScheme)));
-
-            _mockHttpContext.Setup(h => h.RequestServices.GetService(typeof(IAuthenticationService)))
-                .Returns(authServiceMock.Object);
-            _mockConfig.Setup(c => c["StubAuth"]).Returns("true");
-
-            // Act
-            var result = await _sut.SignOut();
-
-            // Assert
-            var signOutResult = result.Should().BeOfType<SignOutResult>().Subject;
-            signOutResult.AuthenticationSchemes.Should()
-                .ContainSingle().Which.Should().Be(CookieAuthenticationDefaults.AuthenticationScheme);
-            signOutResult.Properties.Parameters["id_token"].Should().Be(expectedToken);
-        }
-
-
-        [Test]
-        public void SignOutCleanup_Should_Delete_Auth_Cookie()
-        {
-            // Act
-            _sut.SignOutCleanup();
-
-            // Assert
-            _mockCookies.Verify(c => c.Delete("SFA.DAS.EmployerFeedback.Web.Auth"), Times.Once);
-        }
-
-        [Test]
-        public void Ping_Should_Return_OkResult()
+        public void Ping_ShouldReturnOkResult()
         {
             // Act
             var result = _sut.Ping();
@@ -134,135 +52,63 @@ namespace SFA.DAS.EmployerFeedback.Web.UnitTests.Controllers
             result.Should().BeOfType<OkResult>();
         }
 
-#if DEBUG
         [Test]
-        public void SigninStub_Should_Return_View_With_Configured_Model()
+        public void Index_ShouldReturnView_WhenRunningLocally()
         {
-            // Arrange
-            _mockConfig.Setup(c => c["StubId"]).Returns("stub123");
-            _mockConfig.Setup(c => c["StubEmail"]).Returns("stub@example.com");
-            var returnUrl = "/home";
+            _configurationMock
+                .Setup(c => c["EnvironmentName"])
+                .Returns("LOCAL");
 
-            // Act
-            var result = _sut.SigninStub(returnUrl);
+            var result = _sut.Index() as ViewResult;
 
-            // Assert
-            var viewResult = result.Should().BeOfType<ViewResult>().Subject;
-            var model = viewResult.Model.Should().BeOfType<SignInStubViewModel>().Subject;
-            model.StubId.Should().Be("stub123");
-            model.StubEmail.Should().Be("stub@example.com");
-            model.ReturnUrl.Should().Be(returnUrl);
+            result.Should().NotBeNull();
         }
 
         [Test]
-        public async Task SigninStubPost_Should_SignIn_And_Redirect_To_SignedInStub()
+        public void Index_ShouldReturnView_WhenRunningInDev()
         {
-            // Arrange
-            var model = new SignInStubViewModel
-            {
-                StubId = "abc",
-                StubEmail = "test@unit.com",
-                ReturnUrl = "/return"
-            };
+            _configurationMock
+                .Setup(c => c["EnvironmentName"])
+                .Returns("DEV");
 
-            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim("email", "test@unit.com") }));
+            var result = _sut.Index() as ViewResult;
 
-            _mockStubAuthService.Setup(s => s.GetStubSignInClaims(It.Is<StubAuthUserDetails>(
-                    d => d.Email == "test@unit.com" && d.Id == "abc")))
-                .ReturnsAsync(claimsPrincipal);
-
-            var authServiceMock = new Mock<IAuthenticationService>();
-            var urlHelperFactoryMock = new Mock<IUrlHelperFactory>();
-
-            var serviceProviderMock = new Mock<IServiceProvider>();
-            serviceProviderMock
-                .Setup(sp => sp.GetService(typeof(IAuthenticationService)))
-                .Returns(authServiceMock.Object);
-            serviceProviderMock
-                .Setup(sp => sp.GetService(typeof(IUrlHelperFactory)))
-                .Returns(urlHelperFactoryMock.Object);
-
-            _mockHttpContext.Setup(h => h.RequestServices).Returns(serviceProviderMock.Object);
-            _sut.ControllerContext = new ControllerContext { HttpContext = _mockHttpContext.Object };
-
-            // Act
-            var result = await _sut.SigninStubPost(model);
-
-            // Assert
-            authServiceMock.Verify(a => a.SignInAsync(
-                _mockHttpContext.Object,
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                claimsPrincipal,
-                It.IsAny<AuthenticationProperties>()), Times.Once);
-
-            var redirect = result.Should().BeOfType<RedirectToRouteResult>().Subject;
-            redirect.RouteName.Should().Be(HomeController.SignedInStubGet);
-            redirect.RouteValues["ReturnUrl"].Should().Be("/return");
-        }
-
-
-        [Test]
-        public void SignedInStub_Should_Return_View_With_Model()
-        {
-            // Arrange
-            var returnUrl = "/dashboard";
-
-            // Act
-            var result = _sut.SignedInStub(returnUrl);
-
-            // Assert
-            var viewResult = result.Should().BeOfType<ViewResult>().Subject;
-            viewResult.Model.Should().BeOfType<SignedInStubViewModel>();
-        }
-#endif
-
-        [Test]
-        public void SignOut_Should_Have_Correct_Route_Name()
-        {
-            // Arrange
-            var method = typeof(HomeController)
-                .GetMethods()
-                .Single(m => m.Name == nameof(HomeController.SignOut) && m.DeclaringType == typeof(HomeController));
-
-            // Act
-            var routeAttr = method.GetCustomAttributes(typeof(RouteAttribute), false)
-                .Cast<RouteAttribute>().Single();
-
-            // Assert
-            routeAttr.Name.Should().Be(HomeController.SignoutGet);
-        }
-
-
-#if DEBUG
-        [Test]
-        public void SigninStub_Should_Have_Correct_Route_Name()
-        {
-            var method = typeof(HomeController).GetMethod(nameof(HomeController.SigninStub));
-            var routeAttr = method.GetCustomAttributes(typeof(RouteAttribute), false)
-                .Cast<RouteAttribute>().Single();
-
-            routeAttr.Name.Should().Be(HomeController.SignInStubGet);
+            result.Should().NotBeNull();
         }
 
         [Test]
-        public void SigninStubPost_Should_Have_Correct_Route_Name()
+        public void Index_ShouldRedirectToAccountsLink_WhenNotRunningLocallyOrDev()
         {
-            var method = typeof(HomeController).GetMethod(nameof(HomeController.SigninStubPost));
-            var routeAttr = method.GetCustomAttributes(typeof(RouteAttribute), false)
-                .Cast<RouteAttribute>().Single();
+            _configurationMock
+                .Setup(c => c["EnvironmentName"])
+                .Returns("PROD");
 
-            routeAttr.Name.Should().Be(HomeController.SignInStubPost);
+            var result = _sut.Index() as RedirectResult;
+
+            result.Should().NotBeNull();
+            result.Url.Should().NotBeNullOrEmpty();
         }
 
         [Test]
-        public void SignedInStub_Should_Have_Correct_Route_Name()
+        public void ProvideFeedbackStub_ShouldDeleteCookieAndRedirect()
         {
-            var method = typeof(HomeController).GetMethod(nameof(HomeController.SignedInStub));
-            var routeAttr = method.GetCustomAttributes(typeof(RouteAttribute), false)
-                .Cast<RouteAttribute>().Single();
+            var httpContextMock = new Mock<HttpContext>();
+            var responseMock = new Mock<HttpResponse>();
+            var responseCookiesMock = new Mock<IResponseCookies>();
 
-            routeAttr.Name.Should().Be(HomeController.SignedInStubGet);
+            httpContextMock.Setup(c => c.Response).Returns(responseMock.Object);
+            responseMock.Setup(r => r.Cookies).Returns(responseCookiesMock.Object);
+            _contextAccessorMock.Setup(c => c.HttpContext).Returns(httpContextMock.Object);
+
+            var result = _sut.ProvideFeedbackStub() as RedirectToRouteResult;
+
+            result.Should().NotBeNull();
+            result.RouteName.Should().Be(ProviderController.ProviderSearchGet);
+
+            responseCookiesMock.Verify(c => c.Delete(GovUkConstants.StubAuthCookieName), Times.Once);
+
+            result.RouteValues.Should().ContainKey("encodedAccountId");
+            result.RouteValues["encodedAccountId"].Should().Be("{{hashedAccountId}}");
         }
-#endif
     }
 }
